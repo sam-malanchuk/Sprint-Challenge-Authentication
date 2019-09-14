1b136a3b2ba3cdf6ef0de5b9676ea11928e910b7
sam-malanchuk <malanchuksam@gmail.com>
2019-09-14 14:04:48 -0400

    installed and returns token on register endpoint using jsonwebtoken

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const jwt = require('jsonwebtoken');
 
 const AuthModel = require('./auth-model.js');
 
@@ -6,16 +7,35 @@ router.post('/register', (req, res) => {
   const { body } = req;
 
   AuthModel.insert(body)
-    .then(user => {
-      res.status(201).json({ message: `Welcome ${user.username}, your account has been created!` });
+    .then(createdUser => {
+      if(createdUser) {
+        const token = generateToken(createdUser);
+        res.status(201).json({
+          message: `Welcome ${createdUser.username}, your account has been created!`,
+          token
+        });
+      } else {
+        res.status(500).json({ message: "Account could not be created" });
+      }
     })
     .catch(err => {
-      res.status(500).json({ message: "error adding user" });
+      res.status(500).json({ message: "error creating account" });
     })
 });
 
 router.post('/login', (req, res) => {
   // implement login
 });
+
+function generateToken(user) {
+    const payload = {
+      sub: user.id,
+      username: user.username
+    };
+    const options = {
+      expiresIn: '1d'
+    };
+    return jwt.sign(payload, "the key should never be hardcoded", options); // hardcoding the key just as an example
+}
 
 module.exports = router;
